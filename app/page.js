5c7ba11c47ff21f321d79bb9fd580ad5311ef17a
Sameer negi <negisameer72@gmail.com>
2025-05-27 20:45:09 +0530

    Removed nevbar

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import About from "./components/About/page";
 import Hero from "./components/Hero/page";
-import NavBar from "./components/Navbar/page";
 import Projects from "./components/Projects/page";
 import Contact from "./components/Contact/page";
 import Footer from "./components/Footer/page";
 export default function Home() {
   return (
     <div className="flex flex-col w-full overflow-hidden  ">
-    <NavBar/>
     <Hero/>
     <About/>
     <Projects/>
